e14c8a0dc7b3331b936918163e16df7dd3569d97
robert b. allen <robert.burch.allen@gmail.com>
2014-06-18 13:26:13 -0400

    added traceur

--- common/nodjs_nvm_install.sh
+++ common/nodjs_nvm_install.sh
@@ -11,18 +11,20 @@ nvm alias default "$VERSION"
 echo '# NVM NODEJS' >> ~/.bashrc
 echo 'export NVM_DIR="$HOME/.nvm"' >> ~/.bashrc
 echo 'source $HOME/.nvm/nvm.sh' >> ~/.bashrc
-echo 'nvm use 0.10.28' >> ~/.bashrc
 echo '[[ -r $NVM_DIR/bash_completion ]] && . $NVM_DIR/bash_completion' >> ~/.bashrc
 echo 'PATH=$NVM_BIN:$PATH' >> ~/.bashrc
 . ~/.bashrc
+
 
 echo "install node modules"
 # npm install -g npm
 npm install -g node-gyp
 npm install -g yo gulp
-npm install -g generator-angular generator-gulp-webapp generator-gulp-ng generator-ng-scaffold
+npm install -g traceur
+npm install -g generator-karma generator-angular generator-gulp-webapp generator-gulp-ng generator-ng-scaffold
 npm install -g slush slush-angular slush-webapp
 npm install -g marked feedr
+npm install -g autoprefixer jshint less
 npm install -g generator-server-configs http-server
 npm install -g deployd generator-deployd
 echo "finished installing nodejs"
