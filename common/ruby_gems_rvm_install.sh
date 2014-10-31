0ed3a282a10748ceb6cfc0ee726877771a887de4
robert b. allen <robert.burch.allen@gmail.com>
2014-10-31 16:05:22 -0400

    Update ruby_gems_rvm_install.sh

--- common/ruby_gems_rvm_install.sh
+++ common/ruby_gems_rvm_install.sh
@@ -3,33 +3,27 @@ sudo apt-get install curl
 \curl -L https://get.rvm.io | bash -s stable --ignore-dotfiles
 source ~/.rvm/scripts/rvm
 rvm list known
-rvm install 2.0.0 --autolibs=2
+rvm install 2.1.1 --autolibs=2
 echo 'install missing packages'
-echo 'then run: rvm install 2.0.0 --autolibs=2'
-echo "gem: --no-ri --no-rdoc" > ~/.gemrc
+echo 'then run: rvm install 2.1.1 --autolibs=2'
 echo '## ruby'
 echo ". $HOME/.rvm/scripts/rvm" >> ~/.bashrc
-# echo "PATH=$PATH:$HOME/.rvm/bin" >> ~/.bashrc
-
 
 source ~/.bashrc
 echo "gem: --no-ri --no-rdoc" > ~/.gemrc
 gem update --system
 
-gem install sass bourbon neat bitters
-gem install compass toolkit compass-recipes
-#gem install bundler rake
-# https://github.com/team-sass/toolkit
-gem install compass-colors color-schemer
-gem install foundation semantic-ui-sass
-#gem install animate-sass omg-text
+gem install sass compass bourbon  
 gem install respond-to modular-scale
-#gem install breakpoint breakpoint-slicer
-# cheat git, cheat cheat, cheat bash
+gem install breakpoint breakpoint-slicer
+gem install compass-colors color-schemer
+
 gem install cheat snipcheat
+# cheat git, cheat cheat, cheat bash
 # generate html file from sublime-text snippets
 # snipcheat generate -d Packages/Rails -o ~/rails_cheatsheet.html
-#gem install
 
+#gem install bitters toolkit compass-recipes
+#gem install foundation semantic-ui-sass
 
 # compass create compass-project -r semantic-ui-sass --using semantic-ui
