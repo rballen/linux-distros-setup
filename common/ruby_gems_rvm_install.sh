e8bdfc4dc91960c9e210a262b5aa7d1737d13a4f
robert b. allen <robert.burch.allen@gmail.com>
2015-01-24 15:48:52 -0500

    Update ruby_gems_rvm_install.sh

--- common/ruby_gems_rvm_install.sh
+++ common/ruby_gems_rvm_install.sh
@@ -1,29 +1,47 @@
 #!/bin/bash
-sudo apt-get install git-core curl build-essential python-software-properties sqlite3
-sudo apt-get install zlib1g-dev libssl-dev libreadline-dev libyaml-dev libsqlite3-dev libxml2-dev libxslt1-dev libcurl4-openssl-dev 
-gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3
-\curl -sSL https://get.rvm.io | bash -s stable 
-source ~/.rvm/scripts/rvm
-rvm list known
-rvm install 2.1 --autolibs=read-fail
-echo 'install missing packages'
-rvm install 2.1 --autolibs=read-fail
-echo '## ruby'
-echo ". $HOME/.rvm/scripts/rvm" >> ~/.bashrc
+# setup ruby and gems like sass, compass, bourbon, etc for webdev
+# this more or less works for ubuntu, elementary and arch based distros
+# rvm requirements will list other dependancies
 
-echo "GEM_HOME=/home/ra/.gem/ruby/2.1.0/bin" >> ~/.profile
-echo "PATH=$GEM_HOME:$PATH"
-source ~/.bashrc
-echo "gem: --no-ri --no-rdoc" >> ~/.gemrc
-echo "gem: --user-install" >> ~/.gemrc
-gem update --system
+if [ -f /etc/debian_version ] ; then
+  sudo apt-get install git-core curl build-essential python-software-properties sqlite3
+  sudo apt-get install zlib1g-dev libssl-dev libreadline-dev libyaml-dev libsqlite3-dev libxml2-dev libxslt1-dev libcurl4-openssl-dev 
+elif [ -d /etc/pacman.d ] ; then
+  sudo pacman -Sy
+  sudo pacman -S git curl base-devel
+fi
 
-gem install sass compass bourbon  
-gem install respond-to modular-scale
-gem install breakpoint breakpoint-slicer
+# install rvm, set paths and source
+curl -L get.rvm.io | bash -s stable
+#gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+echo '# ruby' >> ~/.bashrc
+echo '[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"' >> ~/.bashrc
+echo 'export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting' >> ~/.bashrc
+source ~/.rvm/scripts/rvms
+
+# install latest ruby via rvm
+rvm requirements
+rvm list known      # list available rubies 
+rvm install ruby    # install latest ruby
+rvm default
+ruby -v
+which ruby
+rvm list            # list locally installed rubies
+
+# install gems && find them here: https://rubygems.org/
+rvm rubygems current
+echo "gem: --no-document" >> ~/.gemrc    # dont install documentation
+gem install sass compass bourbon
 gem install compass-colors color-schemer
+gem install respond-to modular-scale
+#gem install breakpoint breakpoint-slicer
+#gem install cheat snipcheat
+gem list            # list installed gemsets
 
-gem install cheat snipcheat
+gem update --system && gem update      # update rubygem and installed gems
+
+
+
 # cheat git, cheat cheat, cheat bash
 # generate html file from sublime-text snippets
 # snipcheat generate -d Packages/Rails -o ~/rails_cheatsheet.html
