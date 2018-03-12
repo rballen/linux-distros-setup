297809e99000b3c8f3e9f5998e640848b577fda8
robert b. allen <robert.burch.allen@gmail.com>
2018-03-12 13:32:16 -0400

    Update nodjs_nvm_install.sh

--- common/nodjs_nvm_install.sh
+++ common/nodjs_nvm_install.sh
@@ -1,32 +1,63 @@
-## nodejs, less, sass, yeoman for modern webdev, js dev, mobile dev
-#Visit https://github.com/creationix/nvm to find the latest tag version. Today its - v0.17.3
+#!/bin/bash
+####################################
+# Installs nvm, nodejs and packages. Find the latest versions from below links
+# https://nodejs.org/en/
+# https://github.com/creationix/nvm
+####################################
 
-curl https://raw.githubusercontent.com/creationix/nvm/v0.24.1/install.sh | bash
-echo '# NVM NODEJS' >> ~.bashrc
-echo 'export NVM_DIR="$HOME/.nvm"' >> ~.bashrc
-echo 'source $HOME/.nvm/nvm.sh' >> ~.bashrc
-echo '[[ -r $NVM_DIR/bash_completion ]] && . $NVM_DIR/bash_completion' >> ~.bashrc
-echo 'PATH=$NVM_BIN:$PATH' >> ~.bashrc
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
 
-source ~/.bashrc
-nvm ls-remote       # lists versions - visit nodejs.org to find the official prod version. Today its v0.10.33
-nvm install v0.12.2
-nvm use 0.12.2
-nvm alias default 0.12.2
-which node       # tests all is ok
-# nodejs npm packages
-npm update -g npm
 
-echo "install node modules"
-# npm install -g npm
+nvm install lts/carbon  # installs node 8
+. ~/.profile
+nvm use 8.10.0
+nvm alias default 8.10.0
+sudo ln -s /home/ra/.nvm/versions/node/v8.10.0/bin/node /usr/bin
+npm set progress=false
+npm config set python /usr/bin/python2.7 -g
+
 npm install -g node-gyp
-npm install -g bower grunt-cli gulp yo
-npm install -g generator-karma generator-gulp generator-gulp-webapp
-npm install -g generator-simple-webapp
-npm install -g generator-angular generator-gulp-angular generator-ng-poly generator-mobile
-npm install -g generator-polymer-gulp generator-deployd generator-server-configs
-npm install -g slush slush-webapp slush-angular
-npm install -g traceur vulcanize less jasmine-node
-npm install -g autoprefixer jshint feedr kuler2gpl marked
-echo "finished installing nodejs"
+npm install -g yo
+# npm i -g @storybook/cli
+
+# yarn
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get update
+sudo apt-get install --no-install-recommends yarn
+# add to .bash_aliases alias nodejs=node
+
+yarn global add generator-jhipster
+
+
+### ever changing npm installs as reference 
+
+# npm install -g bower karma yo slush
+# npm install -g gulp
+# npm install -g typescript marked
+# npm install -g node-sass less 
+# npm install -g @angular/cli@latest
+# npm install -g generator-jhipster
+# npm install -g csslint eslint semistandard-format semistandard
+
+
+# npm install -g eslint-config-airbnb-base eslint-plugin-import
+# npm install -g generator-fountain-webapp generator-server-configs generator-webapp
+# npm install -g jhipster jshint karma less
+# npm install -g marked node-sass
+# npm install -g protractor
+# npm install -g semistandard semistandard-format slush
+
+# npm i -g typings@1.3.3 typescript@2.0.2 ts-node@1.3.0
+# npm install -g generator-karma generator-gulp
+# npm install -g generator-simple-webapp
+# npm install -g generator-gulp-angular generator-ng-poly generator-mobile
+# npm install -g generator-deployd generator-server-configs
+# npm install -g generator-tldr-static generator-yeogurt
+# npm install -g slush-webapp slush-angular-gulp
+# npm install -g traceur vulcanize jasmine-node
+# npm install -g autoprefixer jshint feedr kuler2gpl 
+
+# phantomjs 2.0.0
+# https://github.com/bprodoehl/phantomjs/releases/
 
